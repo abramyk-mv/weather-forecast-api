775f391d79e4ee0417f9771cc5b12b4a0a0bc5ed
Maria Abramyk <abramyk.mv@gmail.com>
2020-05-18 23:57:53 +0300

    Move to NODE_ENV.

--- knexfile.js
+++ knexfile.js
@@ -14,4 +14,17 @@ module.exports = {
             directory: './migrations',
         },
     },
+    test: {
+        client: 'mysql',
+        connection: {
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_USE_TEST,
+        },
+        migrations: {
+            tableName: 'knex_migrations',
+            directory: './migrations',
+        },
+    },
 };
